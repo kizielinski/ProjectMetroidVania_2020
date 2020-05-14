09ff458115c0cab4194fbf70b07ea85ea845e905
kizielinski <kiz8980@rit.edu>
2020-05-14 19:12:45 -0400

    Added "Hello Audio!" to AudioManager_2.cs

--- PM/Assets/Scripts/AudioManager_2.cs
+++ PM/Assets/Scripts/AudioManager_2.cs
@@ -7,7 +7,7 @@ public class AudioManager_2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hello Audio!");
     }
 
     // Update is called once per frame
