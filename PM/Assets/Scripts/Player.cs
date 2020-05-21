3e8a66c2c8c4500ab05e6eff2d2f220ff7fbca46
kizielinski <kiz8980@rit.edu>
2020-05-21 16:48:54 -0400

    Added some small changes to Player.cs

--- PM/Assets/Scripts/Player.cs
+++ PM/Assets/Scripts/Player.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class Player : Object
 {
     // Start is called before the first frame update
+
+    
+    int health;
+    int level; //Used to track when cutscenes should be played. Only played once for each area.
+    int[] weapon; //[0] = Weapon Type, [1] = Current Rounds, [2] = Current Ammo;
+    //int comboCounter; //Used for hand to hand combat
+
     void Start()
     {
-
+        name = "ARC";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(health <= 0)
+        {
+            //End Game
+        }
     }
+
     protected override void Move()
     {
         base.Move();
