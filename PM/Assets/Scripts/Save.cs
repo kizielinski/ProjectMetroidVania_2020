1da0a27a41811cfd0798ea7e0a8317a812dbb646
kizielinski <kiz8980@rit.edu>
2020-05-21 17:45:59 -0400

    Added Save.cs which makes use of serialization to take data from a class and save it. Has full implementation, and will be expanded as we finish round out character. Player.cs still **needs** a save and load method implemented.

--- PM/Assets/Scripts/Save.cs
+++ PM/Assets/Scripts/Save.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
-public class Save : MonoBehaviour
+public static class Save
 {
-    // Start is called before the first frame update
-    void Start()
+    public static void SavePlayer(Player player)
     {
-        
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream newStream = new FileStream(Application.persistentDataPath + "/player.local", FileMode.Create);
+
+        PlayerData data = new PlayerData(player);
+
+        formatter.Serialize(newStream, player);
+        newStream.Close();
     }
 
-    // Update is called once per frame
-    void Update()
+    public static int[] LoadPlayer()
     {
-        
+        if(File.Exists(Application.persistentDataPath + "/player.local"))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream inStream = new FileStream(Application.persistentDataPath + "/player.local", FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(inStream) as PlayerData;
+            //Or below since its just a cast
+            //PlayerData data = (PlayerData)formatter.Deserialize(openStream);
+            inStream.Close();
+            return data.playerInfo;
+        }
+
+        return null;
     }
+}
+
+[Serializable]
+public class PlayerData
+{
+    public int[] playerInfo;
+
+    public PlayerData(Player player)
+    {
+
+    }
+    //Save player info, once we determine the final stats
 }
