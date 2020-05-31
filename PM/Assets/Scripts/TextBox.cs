f5a1a8d2c642daaff26368d2ab2cd293181a79b9
Will <wsb6157@rit.edu>
2020-05-31 16:04:11 -0400

    optimized and reworked TextBox

--- PM/Assets/Scripts/TextBox.cs
+++ PM/Assets/Scripts/TextBox.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,40 +8,43 @@ using UnityEngine;
 // 5/31/2020
 
 // Reference: https://gamedev.stackexchange.com/questions/138485/how-to-make-a-text-box-where-text-types-smoothly
-
 public class TextBox : MonoBehaviour
 {
-    string textToType;  // The text to type onscreen
-    float singleKeyDuration; // How long to wait before typing the next character
-    string textDisplayed; // The text that is currently being shown to the player
-    float timeStarted; // The time the text has begun to show
+    public string textToType;   // The text to type onscreen
+    public float timeToType;    // Speed of typing
+
+    float singleKeyDuration;    // How long to wait before typing the next character
+    string textDisplayed;       // The text that is currently being shown to the player
+    float timeStarted;          // The time the text has begun to show
+    int charNumber;             // Number of characters in a string
+    bool finished;              // Determines if text has finished typing
 
     // Start is called before the first frame update
     void Start()
     {
-        // Initializing text typing variables
-        TypeText("We know they sent you! Please just let us go and you’ll never hear from us again. Please, I'm begging you don’t continue their tyranny. No! Don’t…", 10);
+        charNumber = textToType.Length;                 // Get the number of characters in the textToType   
+        singleKeyDuration = timeToType / charNumber;    // Calculate how long to wait before typing the next char
+        textDisplayed = "";                             // Set textDisplayed to an empty string to start
+        timeStarted = Time.time;                        // Set the timeStarted to the current time
+        finished = false;                               // Set finisdhed to false
     }
 
     // Update is called once per frame
     void Update()
     {
-        var numOfCharsSoFar = (Time.time - timeStarted) / singleKeyDuration; // Get the number of chars that should be displayed based on time typing started and time to type a char
-        textDisplayed = textToType.Substring(0, (int)numOfCharsSoFar); // Update which char is displayed next based on numOfCharsSoFar
-    }
+        // Only update text display if text has not finished typing
+        if (!finished)
+        {
+            var numOfCharsSoFar = (Time.time - timeStarted) / singleKeyDuration;    // Get the number of chars that should be displayed based on time typing started and time to type a char
 
-    /// <summary>
-    /// Initialized the variables used for typing text based on the text contents and the time it takes to type text
-    /// </summary>
-    /// <param name="text"> The text to display </param>
-    /// <param name="timeToType"> The time it takes to type one character in the text </param>
-    public void TypeText(string text, float timeToType)
-    {
-        textToType = text;
-        var charNumber = textToType.Length;
-        singleKeyDuration = timeToType / charNumber;
-        textDisplayed = "";
-        timeStarted = Time.time;
+            textDisplayed = textToType.Substring(0, (int)numOfCharsSoFar);          // Update which char is displayed next based on numOfCharsSoFar
+
+            // The number of characters that has been typed is equal to the number of characters in the text, the text has finished typing
+            if (charNumber == (int)numOfCharsSoFar)
+            {
+                finished = true;
+            }
+        }
     }
 
     // Displays text on GUI
