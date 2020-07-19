9067cb1519cf644b8edc2f0e5922f918c219c706
SeanLynch717 <sdlgamerguy@gmail.com>
2020-07-19 14:32:38 -0400

    Enemy Class
    Created enemy class, overrided Update, created Seek method. Still needs to be attatched to a prefab.

---
+++ PM/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Object
+{
+    public GameObject _player;
+    /// <summary>
+    /// Enemy seeks as long as it is this distance from the target object or farther.
+    /// </summary>
+    private float _seekRadius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _seekRadius = 1.5f;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        this.Seek(_player.transform.position);
+    }
+
+    private void Seek(Vector2 seekPos)
+    {
+        float squaredDist = Mathf.Pow(this.transform.position.x - seekPos.x, 2) + Mathf.Pow(this.transform.position.y - seekPos.y, 2);
+        if (squaredDist < Mathf.Pow(_seekRadius, 2))
+        {
+            StopHorizontalMotion();
+            return;
+        }
+        Vector2 direction = (this.transform.position - (Vector3)seekPos).normalized;
+        Vector2 seekForce = direction * Mathf.Pow((squaredDist - Mathf.Pow(_seekRadius, 2)), 2);
+        ApplyForce(seekForce);
+    }
+}
